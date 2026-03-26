df25b1872f53f8f11099f6f3d0d4d66ca398e2dd
hbizier <hbizier@binghamton.edu>
2026-03-26 15:18:11 -0400

    updated README

--- submit/lab9-sol/inexact/inexact.c
+++ submit/lab9-sol/inexact/inexact.c
@@ -7,11 +7,8 @@ typedef float Real;
 typedef double Real;
 #endif
 
-#define TODO 0
-
-// replace TODO below with 0.1
 int main() {
-  const Real x = TODO;             //value being summed repeatedly
+  const Real x = 1.0;             //value being summed repeatedly
   const int n = ceil(1.0/x);      //how many additions to sum to 1.0
   Real sum = 0;
   for (int j = 0; j < n; j++) {
--- submit/lab9-sol/nan/nan.c
+++ submit/lab9-sol/nan/nan.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
-#define TODO 9999.99
-
 int main() {
-  double v = TODO;
+  double v = 0.0;
   double x = v/v;
   const char *neq = (x != x) ? "true" : "false";
   printf("v = %g; x = %g; \"%s\" must be \"true\"\n", v, x, neq);
